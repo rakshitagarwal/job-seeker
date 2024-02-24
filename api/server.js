076b8ce53330c88997ccfa0489563e22caa03d17
Rakshit Agarwal <rakshitag13@gmail.com>
2024-02-24 11:55:47 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:4000'],
+    origin: ['http://localhost:5173', 'http://localhost:4000', 'https://job-seeker-j54r.onrender.com'],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
